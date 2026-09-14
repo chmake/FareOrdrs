2f88f2b717a88af0bf4c1bd1416f015af41810b8
chmake <hammadmer7@gmail.com>
2026-09-14 16:29:54 +0000

    Update FareOrders.java

--- src/main/java/com/fare/fareorders/FareOrders.java
+++ src/main/java/com/fare/fareorders/FareOrders.java
@@ -5,10 +5,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class FareOrders extends JavaPlugin {
 
     private static FareOrders instance;
+    private OrderManager orderManager;
 
     @Override
     public void onEnable() {
         instance = this;
+
+        orderManager = new OrderManager();
+
+        if (getCommand("orders") != null) {
+            getCommand("orders").setExecutor(new OrdersCommand(this));
+        }
 
         getLogger().info("FareOrders enabled.");
     }
@@ -16,9 +23,14 @@ public final class FareOrders extends JavaPlugin {
     @Override
     public void onDisable() {
         getLogger().info("FareOrders disabled.");
+        instance = null;
     }
 
     public static FareOrders getInstance() {
         return instance;
+    }
+
+    public OrderManager getOrderManager() {
+        return orderManager;
     }
 }

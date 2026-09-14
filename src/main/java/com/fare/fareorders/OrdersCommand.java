9229a8615c61f1698cfd3f9063ae3503512ea3a3
chmake <hammadmer7@gmail.com>
2026-09-14 16:41:28 +0000

    Connect /orders to the finished GUI

--- src/main/java/com/fare/fareorders/OrdersCommand.java
+++ src/main/java/com/fare/fareorders/OrdersCommand.java
@@ -6,25 +6,10 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public final class OrdersCommand implements CommandExecutor {
-
     private final FareOrders plugin;
-
-    public OrdersCommand(FareOrders plugin) {
-        this.plugin = plugin;
-    }
-
-    @Override
-    public boolean onCommand(
-            CommandSender sender,
-            Command command,
-            String label,
-            String[] args
-    ) {
-        if (!(sender instanceof Player player)) {
-            sender.sendMessage("Only players can use /orders.");
-            return true;
-        }
-
+    public OrdersCommand(FareOrders plugin){this.plugin=plugin;}
+    @Override public boolean onCommand(CommandSender sender,Command command,String label,String[] args){
+        if(!(sender instanceof Player player)){sender.sendMessage("Only players can use /orders.");return true;}
         plugin.getGui().openMainMenu(player);
         return true;
     }

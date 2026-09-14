d523e1a4ca83c8098bbd41032946e0dc81c83220
chmake <hammadmer7@gmail.com>
2026-09-14 16:36:55 +0000

    Wire GUI and order sessions into plugin lifecycle

--- src/main/java/com/fare/fareorders/FareOrders.java
+++ src/main/java/com/fare/fareorders/FareOrders.java
@@ -3,6 +3,10 @@ package com.fare.fareorders;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public final class FareOrders extends JavaPlugin {
 
     private static FareOrders instance;
@@ -10,6 +14,8 @@ public final class FareOrders extends JavaPlugin {
     private OrderManager orderManager;
     private Database database;
     private EconomyManager economyManager;
+    private GUI gui;
+    private final Map<UUID, OrderSession> orderSessions = new HashMap<>();
 
     @Override
     public void onEnable() {
@@ -20,6 +26,7 @@ public final class FareOrders extends JavaPlugin {
         orderManager = new OrderManager();
         database = new Database(this);
         economyManager = new EconomyManager(this);
+        gui = new GUI(this);
 
         try {
             database.connect();
@@ -41,11 +48,18 @@ public final class FareOrders extends JavaPlugin {
             ordersCommand.setExecutor(new OrdersCommand(this));
         }
 
+        getServer().getPluginManager().registerEvents(
+                new OrderListener(this),
+                this
+        );
+
         getLogger().info("FareOrders enabled.");
     }
 
     @Override
     public void onDisable() {
+        orderSessions.clear();
+
         if (database != null) {
             database.close();
         }
@@ -69,5 +83,23 @@ public final class FareOrders extends JavaPlugin {
 
     public EconomyManager getEconomyManager() {
         return economyManager;
+    }
+
+    public GUI getGui() {
+        return gui;
+    }
+
+    public OrderSession getOrderSession(UUID uuid) {
+        return orderSessions.get(uuid);
+    }
+
+    public OrderSession createOrderSession(UUID uuid) {
+        OrderSession session = new OrderSession(uuid);
+        orderSessions.put(uuid, session);
+        return session;
+    }
+
+    public void removeOrderSession(UUID uuid) {
+        orderSessions.remove(uuid);
     }
 }
